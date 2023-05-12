052151a64b1af047c1faa4ff6046c6917bd2c632
Eloi <recycled_loops@hotmail.com>
2023-05-12 13:33:02 +0200

    use new fonts

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import { Alchemy, Network } from 'alchemy-sdk';
 import {
   ChakraProvider,
   Flex,
-  Center,
-  Stack,
-  Container,
   Box,
   Text,
   Button,
@@ -16,7 +13,7 @@ import {
   Heading,
   ListIcon,
 } from '@chakra-ui/react';
-import chakraTheme from '@chakra-ui/theme';
+import theme from './theme';
 // import { Center, Wrapper, TransactionsWrapper } from './components';
 import './App.css';
 
@@ -80,7 +77,7 @@ function App() {
 
   return (
     <React.Fragment>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Flex>
           <Flex direction="column" w="100vw" p="2">
             {/* <Box flex="1" h="300px">
@@ -89,167 +86,87 @@ function App() {
             <Button colorScheme="teal" variant="solid" mb="2" onClick={handleGetLatestTransactions}>
               Get most recently mined block
             </Button>
-            <Center bg="#262525" h="100vh" color="white">
-              <Flex>
-                <Flex direction="column">
-                  <Box
-                    style={{ border: '1px solid black', overflowY: 'scroll', borderRadius: '15px' }}
-                    w="1100px"
-                    h="600px"
-                    align="center"
+            {/* <Center bg="#262525" h="100vh" color="white"> */}
+            <Flex bg="#262525" h="100vh" color="white" justify="center" pt="6">
+              <Flex direction="column">
+                <Box
+                  style={{ border: '1px solid black', overflowY: 'scroll', borderRadius: '15px' }}
+                  w="1100px"
+                  h="600px"
+                  align="center"
+                >
+                  <Heading as="h6" mb="4">
+                    Block Number: {blockDetails?.number}
+                  </Heading>
+                  <Text mb="2">Click to display block transactions:</Text>
+                  <Button
+                    size="md"
+                    colorScheme="teal"
+                    mb="3"
+                    onClick={handleToggleBlockTransactions}
                   >
-                    <Heading as="h6" mb="4">
-                      Block Number: {blockDetails?.number}
-                    </Heading>
-                    <Text mb="2">Click to display block transactions:</Text>
-                    <Button
-                      size="md"
-                      colorScheme="teal"
-                      mb="3"
-                      onClick={handleToggleBlockTransactions}
-                    >
-                      {blockDetails?.number}
-                    </Button>
-                    <Flex justify="center" align="center">
-                      <Text as="b">Parent hash: </Text>
-                      <Text>{blockDetails?.parentHash}</Text>
-                    </Flex>
-                    <Flex justify="start" mt="5">
-                      <List>
-                        <Text mb="3">Block transactions:</Text>
-
-                        {blockTransactions
-                          ? blockDetails?.transactions?.map(
-                              (item: string | undefined, index: React.Key | null | undefined) => {
-                                return (
-                                  <ListItem>
-                                    <ListIcon as="CheckIcon" color="green.500" />
-                                    <Button
-                                      size="sm"
-                                      colorScheme="yellow"
-                                      mb="1"
-                                      onClick={() => handleTransactionInfo(index)}
-                                    >
-                                      {item}
-                                    </Button>
-                                  </ListItem>
-                                );
-                              },
-                            )
-                          : null}
-                      </List>
-                    </Flex>
-                    {/* {blockTransactions
-                      ? blockDetails?.transactions?.map(
-                          (item: string | undefined, index: React.Key | null | undefined) => {
-                            return (
-                              <TransactionsWrapper key={index}>
-                                <Button
-                                  key={index}
-                                  text={item}
-                                  theme="secondary"
-                                  onClick={() => handleTransactionInfo(index)}
-                                />
-                              </TransactionsWrapper>
-                            );
-                          },
-                        )
-                      : null} */}
-                  </Box>
-                  <Flex direction="column" mt="5">
-                    <Text>
-                      <Text>Transaction hash:</Text> {singleTransactionInfo?.transactionHash}
-                    </Text>
-                    <Text mt="3">
-                      <Text>From:</Text> {singleTransactionInfo?.from}
-                    </Text>
-                    <Text mt="3">
-                      <Text>To:</Text> {singleTransactionInfo?.to}
-                    </Text>
-                    <Text mt="3">
-                      <Text>Confirmations: </Text>
-                      {singleTransactionInfo?.confirmations}
-                    </Text>
+                    {blockDetails?.number}
+                  </Button>
+                  <Flex justify="center" align="center">
+                    <Text as="b">Parent hash: </Text>
+                    <Text>{blockDetails?.parentHash}</Text>
                   </Flex>
-
-                  <Input
-                    type="text"
-                    label="type hash to see balance"
-                    onChange={handleInputValue}
-                    onKeyDown={handleOnKeyPressed}
-                  />
-
-                  <Button text="Click to see balance :)" onClick={handleBalance} />
-                  {balance}
+                  <Flex justify="start" mt="5">
+                    <List>
+                      <Text mb="3">Block transactions:</Text>
+                      {blockTransactions
+                        ? blockDetails?.transactions?.map(
+                            (item: string | undefined, index: React.Key | null | undefined) => {
+                              return (
+                                <ListItem>
+                                  <ListIcon as="CheckIcon" color="green.500" />
+                                  <Button
+                                    size="sm"
+                                    colorScheme="yellow"
+                                    mb="1"
+                                    onClick={() => handleTransactionInfo(index)}
+                                  >
+                                    {item}
+                                  </Button>
+                                </ListItem>
+                              );
+                            },
+                          )
+                        : null}
+                    </List>
+                  </Flex>
+                </Box>
+                <Flex direction="column" mt="5">
+                  <Text>
+                    <Text>Transaction hash:</Text> {singleTransactionInfo?.transactionHash}
+                  </Text>
+                  <Text mt="3">
+                    <Text>From:</Text> {singleTransactionInfo?.from}
+                  </Text>
+                  <Text mt="3">
+                    <Text>To:</Text> {singleTransactionInfo?.to}
+                  </Text>
+                  <Text mt="3">
+                    <Text>Confirmations: </Text>
+                    {singleTransactionInfo?.confirmations}
+                  </Text>
                 </Flex>
+
+                <Input
+                  color="teal"
+                  _placeholder={{ color: 'inherit' }}
+                  placeholder="Type address or ENS to see balance"
+                  onChange={handleInputValue}
+                  onKeyDown={handleOnKeyPressed}
+                />
+
+                <Button text="Click to see balance :)" onClick={handleBalance} />
+                {balance}
               </Flex>
-            </Center>
+            </Flex>
+            {/* </Center> */}
           </Flex>
         </Flex>
-        {/* <Center>
-          <Wrapper>
-            <Typography variant="h3">Block Number: {blockDetails?.number}</Typography>
-
-            <p>Click to display block transactions:</p>
-            <Button
-              type="button"
-              size="regular"
-              theme="primary"
-              text={blockDetails?.number}
-              onClick={handleToggleBlockTransactions}
-              style={{ minHeight: '30px' }}
-            />
-
-            <p>
-              <b>Parent hash:</b> {blockDetails?.parentHash}
-            </p>
-            <b>Block transactions:</b>
-
-            {blockTransactions
-              ? blockDetails?.transactions?.map(
-                  (item: string | undefined, index: React.Key | null | undefined) => {
-                    return (
-                      <TransactionsWrapper key={index}>
-                        <Button
-                          key={index}
-                          text={item}
-                          theme="secondary"
-                          onClick={() => handleTransactionInfo(index)}
-                        />
-                      </TransactionsWrapper>
-                    );
-                  },
-                )
-              : null}
-          </Wrapper>
-          <div>
-            <p>
-              <Typography variant="subtitle2">Transaction hash:</Typography>{' '}
-              {singleTransactionInfo?.transactionHash}
-            </p>
-            <p>
-              <Typography variant="subtitle2">From:</Typography> {singleTransactionInfo?.from}
-            </p>
-            <p>
-              <Typography variant="subtitle2">To:</Typography> {singleTransactionInfo?.to}
-            </p>
-            <p>
-              <Typography variant="subtitle2">Confirmations: </Typography>
-              {singleTransactionInfo?.confirmations}
-            </p>
-          </div>
-
-          <Input
-            type="text"
-            label="type hash to see balance"
-            value=""
-            onChange={handleInputValue}
-            onKeyDown={handleOnKeyPressed}
-          />
-
-          <Button text="Click to see balance :)" onClick={handleBalance} />
-          {balance}
-        </Center> */}
       </ChakraProvider>
     </React.Fragment>
   );
